b85fd0d28f16c7df10584d919e87cd8ad965feb6
Leonid Lovsky <leonid.lovsky@yandex.ru>
2022-09-03 15:44:03 +0300

    commit

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -1,16 +1,27 @@
 from rest_framework import permissions
 
 
-class AuthorOrReadOnly(permissions.BasePermission):
+class ReadOnly(permissions.BasePermission):
 
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-        else:
-            return request.user.is_authenticated
+        return False
 
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
-        else:
-            return obj.owner == request.user
+        return False
+
+
+class IsAuthorOrReadOnly(permissions.BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_authenticated
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.owner == request.user
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from rest_framework.generics import get_object_or_404
 from rest_framework.pagination import LimitOffsetPagination
 
-from api.permissions import AuthorOrReadOnly
+from api.permissions import IsAuthorOrReadOnly, ReadOnly
 from api.serializers import PostSerializer, CommentSerializer, GroupSerializer
 from posts.models import Post, Comment, Group
 
@@ -10,12 +10,13 @@ from posts.models import Post, Comment, Group
 class GroupViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
+    permission_classes = (ReadOnly,)
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = (AuthorOrReadOnly,)
+    permission_classes = (IsAuthorOrReadOnly,)
     pagination_class = LimitOffsetPagination
 
     def perform_create(self, serializer):
@@ -25,7 +26,7 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = (AuthorOrReadOnly,)
+    permission_classes = (IsAuthorOrReadOnly,)
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
