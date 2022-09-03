a4bd1004a0a2ef9a55f6fd9efe585a3cc24cf0d2
Leonid Lovsky <leonid.lovsky@yandex.ru>
2022-09-03 12:55:51 +0300

    commit

--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -4,13 +4,21 @@ from django.db import models
 User = get_user_model()
 
 
+class Group(models.Model):
+    pass
+
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='posts')
-    image = models.ImageField(
-        upload_to='posts/', null=True, blank=True)
+    group = models.ForeignKey(
+        Group, on_delete=models.CASCADE, related_name='posts',
+        null=True, black=True)
+
+    # image = models.ImageField(
+    #     upload_to='posts/', null=True, blank=True)
 
     def __str__(self):
         return self.text
