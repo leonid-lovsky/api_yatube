13c824f4c8fda799c0381a187aa7d30717f6e5e4
Leonid Lovsky <leonid.lovsky@yandex.ru>
2022-09-03 14:12:52 +0300

    commit

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,21 +1,27 @@
 from rest_framework import serializers
 
-from posts.models import Comment, Post
+from posts.models import Comment, Post, Group
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        slug_field='username', read_only=True)
+    author = serializers.SlugRelatedField(slug_field='username')
 
     class Meta:
         fields = '__all__'
         model = Post
+        read_only_fields = ('author', 'pub_date')
 
 
 class CommentSerializer(serializers.ModelSerializer):
-    author = serializers.SlugRelatedField(
-        slug_field='username', read_only=True)
+    author = serializers.SlugRelatedField(slug_field='username')
 
     class Meta:
         fields = '__all__'
         model = Comment
+        read_only_fields = ('author', 'created')
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = '__all__'
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -11,3 +11,6 @@ class PostViewSet(viewsets.ModelViewSet):
     serializer_class = PostSerializer
     permission_classes = (AuthorOrReadOnly,)
     pagination_class = LimitOffsetPagination
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
